afd8d904f07e67a734f117aef89bf8a8dd79fc61
Oleksandr Bakun <alexanderbakun@gmail.com>
2017-10-25 21:55:28 +0200

    adding node_modules and setting package.json

---
+++ README.md
@@ -0,0 +1,2 @@
+# paprika-cli
+A cli tool helper for the paprika framework
---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+'use strict';
+
+var myvar = require ("cat-names");
+
+console.log("TEST TEST TEST");
+catNames.random;
